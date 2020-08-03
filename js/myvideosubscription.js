ca7ec05ef4da00bed41c5faac433b5ce6f2c32ad
Joyvishek <joyghosh2609@gmail.com>
2020-08-03 06:21:38 +0530

    youtube no video data output results from api

--- js/myvideosubscription.js
+++ js/myvideosubscription.js
@@ -1,67 +1,75 @@
-// var CLIENT_ID = '188720940383-9h1q9rc44f0g0llre09g68b26301l81h.apps.googleusercontent.com';
-// var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest"];
-var SCOPE = 'https://www.googleapis.com/auth/youtube.readonly';
+var API_KEY = 'AIzaSyAzJrieP_7_MRmqNCWFIE18Cxg8tHUiuog';
+var CLIENT_ID = '188720940383-u66oaerd2d5uggtsrjrt70501g3q3bjt.apps.googleusercontent.com';
+var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest"];
+var SCOPES =  'https://www.googleapis.com/auth/youtube.force-ssl';
 
 var authorizeButton = document.getElementById('authorize-button');
 var signoutButton = document.getElementById('signout-button');
 var content = document.getElementById('content');
 var channelForm = document.getElementById('channel-form');
-var channelInput = document.getElementById('channel-input');
+// var channelInput = document.getElementById('channel-input');
 var videoContainer = document.getElementById('video-container');
 
-var defaultChannel = 'The IndianFirebolt';
+var defaultChannel = 'googledevelopers';
 
-channelForm.addEventListener('submit', e => {
-    e.preventDefault();
-    var channel = channelInput.value;
-    getChannel(channel);
-});
+// channelForm.addEventListener('submit', e => {
+//     e.preventDefault();
+//     var channel = channelInput.value;
+//     getChannel(channel);
+// });
 
-function handleClinetLoad() {
+function handleClientLoad() {
     gapi.load('client:auth2', initClient);
-}
+  }
 
 function initClient() {
-    var discoveryUrl = 'https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest';
     gapi.client.init({
-        // discoveryDocs: DISCOVERY_DOCS,
-        // clientId: CLIENT_ID,
-        // scope: SCOPES
-        'apiKey': 'AIzaSyAzJrieP_7_MRmqNCWFIE18Cxg8tHUiuog',
-        'clientId': '188720940383-9h1q9rc44f0g0llre09g68b26301l81h.apps.googleusercontent.com',
-        'discoveryDocs': [discoveryUrl],
-        'scope': SCOPE
-    }).then(()=> {
-        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
-        updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-        authorizeButton.onclick = handleAuthClick;
-        signoutButton.onclick = handleSignoutClick;
+      apiKey: API_KEY,
+      clientId: CLIENT_ID,
+      discoveryDocs: DISCOVERY_DOCS,
+      scope: SCOPES
+    }).then(function () {
+      // Listen for sign-in state changes.
+      gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+      // Handle the initial sign-in state.
+      updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+      authorizeButton.onclick = handleAuthClick;
+      signoutButton.onclick = handleSignoutClick;
+    }, function(error) {
+      console.log(error);
     });
-}
+  }
 
 function updateSigninStatus(isSignedIn) {
     if (isSignedIn) {
         authorizeButton.style.display = 'none';
         signoutButton.style.display = 'block';
-        content.style.display = 'block';
-        videoContainer.style.display = 'block';
+        // content.style.display = 'block';
+        // videoContainer.style.display = 'block';
         getChannel(defaultChannel);
     } else {
         authorizeButton.style.display = 'block';
         signoutButton.style.display = 'none';
-        content.style.display = 'none';
-        videoContainer.style.display = 'none';
+        // content.style.display = 'none';
+        // videoContainer.style.display = 'none';
 
     }
 }
 
-function handleAuthClick() {
+function handleAuthClick(event) {
     gapi.auth2.getAuthInstance().signIn();
+  }
+
+function handleSignoutClick(event) {
+   gapi.auth2.getAuthInstance().signOut();
 }
 
-function handleSignoutClick() {
-    gapi.auth2.getAuthInstance().signOut();
-}
+// function appendPre(message) {
+//     var pre = document.getElementById('content');
+//     var textContent = document.createTextNode(message + '\n');
+//     pre.appendChild(textContent);
+// }
 
 function showChannelData(data) {
     var channelData = document.getElementById('channel-data');
@@ -73,10 +81,9 @@ function getChannel(channel) {
         part: 'snippet,contentDetails,statistics',
         forUsername: channel
     })
-    .then(response => {
+    .then((response) => {
         console.log(response);
         var channel = response.result.items[0];
-
         var output =`
         <ul class="collection">
             <li class="collection-item">Title: ${channel.snippet.title}</li>
@@ -90,36 +97,71 @@ function getChannel(channel) {
         `;
         showChannelData(output);
 
-        var playlistId = channel.contentDetals.relatedPlaylists.uploads;
+        var playlistId = channel.contentDetails.relatedPlaylists.uploads;
+        console.log(playlistId);
         requestVideoPlaylist(playlistId);
-    })
-    .catch(err => alert('No channel'));
-}
-
-function requestVideoPlaylist(playlistId){
-    var requestOptions = {
-        playlistId: playlistId,
-        part: 'snippet',
-        maxResults: 10
-    }
-
-    var request = gapi.client.youtube.playlistItems.list(requestOptions);
-    request.execute(response => {
-        console.log(response);
-        var playlistItems = response.result.items;
-        if(playlistItems) {
-            let = '<br><h4 class="center-align">Latest Videos</h4>';
-            playlistItems.forEach(item => {
-                var videoId = item.snippet.resourceId.videoId;
-                output += `
-                    <div class="col s3">
-                    <iframe width="100%" height="auto" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                    </div> 
-                `;
-            });
-            videoContainer.innerHTML = output;
-        } else {
-            videoContainer.innerHTML = 'No Uploaded Videos';
-        }
     });
 }
+
+function requestVideoPlaylist(playlistId) {
+    console.log(playlistId);
+    var requestOptions = {
+      playlistId: playlistId,
+      part: 'snippet',
+      maxResults: 12
+    };
+  
+    var request = gapi.client.youtube.playlistItems.list(requestOptions);
+  
+    request.execute((response) => {
+      console.log(response);
+      var playListItems = response.result.items;
+      if (playListItems) {
+        let output = '<br><h4 class="center-align">Latest Videos</h4>';
+  
+        playListItems.forEach(item => {
+          var videoId = item.snippet.resourceId.videoId;
+  
+          output += `
+            <div class="col s3">
+            <iframe width="100%" height="auto" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+            </div>
+          `;
+        });
+  
+        videoContainer.innerHTML = output;
+      } else {
+        videoContainer.innerHTML = 'No Uploaded Videos';
+      }
+    });
+  }
+
+//   function requestVideoPlaylist(playlistId){
+//     gapi.client.youtube.playlistItems.list({
+//         playlistId: playlistId,
+//         part: 'snippet',
+//         maxResults: 12
+//     })
+//     .then((response) => {
+//         console.log(response);
+//           var playListItems = response.result.items;
+//           if (playListItems) {
+//             let output = '<br><h4 class="center-align">Latest Videos</h4>';
+      
+//             playListItems.forEach(item => {
+//               var videoId = item.snippet.resourceId.videoId;
+      
+//               output += `
+//                 <div class="col s3">
+//                 <iframe width="100%" height="auto" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+//                 </div>
+//               `;
+//             });
+            
+//             videoContainer.innerHTML = output;
+//           } else {
+//             videoContainer.innerHTML = 'No Uploaded Videos';
+//           }
+//     });
+//   }
+  
